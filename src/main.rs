cee324437078473298444309e821b59f5458502f
GlorifiedPig <ryan@glorifiedstudios.com>
2024-01-30 15:43:14 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+
+use socketioxide::{extract::{Data, SocketRef}, SocketIo};
+use axum::{routing::get, response, Router};
+use std::fs;
+
+async fn index() -> response::Html<String> {
+    let html_content: String = fs::read_to_string("src/terminal.html").expect("Unable to read file");
+    response::Html(html_content)
+}
+
+#[tokio::main]
+async fn main() {
+    let (layer, io) = SocketIo::new_layer();
+
+    io.ns("/", |s: SocketRef| {
+        s.on("command", |s: SocketRef, Data::<String>(data)| {
+            s.emit("result", format!("Received {}", data)).ok();
+        });
+    });
+
+    let app: Router = Router::new()
+    .route("/", get(index))
+    .layer(layer);
+
+    let listener = tokio::net::TcpListener::bind("localhost:8000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
